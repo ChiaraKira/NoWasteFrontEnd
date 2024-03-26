5160ca31b15439479924b82da60a5b8cc1a62760
sapli97 <sarapileio@gmail.com>
2024-03-26 11:17:56 +0100

    inserito componente di blogNoWaste da implementare animazione degli span

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,6 +15,7 @@ import {MatIconModule} from '@angular/material/icon';
 import {MatDividerModule} from '@angular/material/divider';
 import {MatButtonModule} from '@angular/material/button';
 import { CategorieComponent } from './categorie/categorie.component';
+import { BlogComponent } from './blog/blog.component';
 
 @NgModule({
   declarations: [
@@ -24,7 +25,8 @@ import { CategorieComponent } from './categorie/categorie.component';
     CardComponent,
     HeaderHomeComponent,
     BottoneComponent,
-    CategorieComponent
+    CategorieComponent,
+    BlogComponent
   ],
   imports: [
     BrowserModule,
