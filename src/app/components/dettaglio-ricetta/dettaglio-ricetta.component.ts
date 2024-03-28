835f7a17eb1e8bb9f301fe2b8083a84f7f7b67fc
sapli97 <sarapileio@gmail.com>
2024-03-28 16:17:31 +0100

    inserito service per ricettaDettaglio

--- src/app/components/dettaglio-ricetta/dettaglio-ricetta.component.ts
+++ src/app/components/dettaglio-ricetta/dettaglio-ricetta.component.ts
@@ -1,9 +1,7 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Component, Input, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
 import { Ricetta } from 'src/app/model/ricetta';
-import { DettaglioRicettaService } from 'src/app/services/dettaglio-ricetta.service';
-import { RicetteService } from 'src/app/services/ricette.service';
+
 
 @Component({
   selector: 'app-dettaglio-ricetta',
@@ -13,38 +11,45 @@ import { RicetteService } from 'src/app/services/ricette.service';
 export class DettaglioRicettaComponent  {
 
 
+  // stringaDalBackend = this.ricetta?.istruzioni;
+  // arrayValori: string[] ;
+
+  @Input() ricetta?: Ricetta;
+
+  constructor(private http: HttpClient) {
+    // this.arrayValori? = this.stringaDalBackend.split("'\'") ;
+  }
+
+  ngOnInit(): void {
+    this.getRicetta();
+  }
+
+  getRicetta(): void {
+    var token = sessionStorage.getItem("token");
+
+    if (!token) {
+      token = "admin-2";
+    }
+
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+      'token': token
+    });
+
+    const idRicetta = 2; // Assuming you want to fetch recipe with id 2
+
+    this.http.get<Ricetta>("http://localhost:8080/api/recipe/recipeById?id=" + idRicetta, { headers }).subscribe(
+      (response: Ricetta) => {
+        this.ricetta = response;
+      },
+      (error) => {
+        console.error('Error fetching recipe:', error);
+      }
+    );
+  }
 
 
+ 
+  
 
-  // @Input() ricetta?: Ricetta;
-
-  // constructor(private http: HttpClient) {}
-
-  // ngOnInit(): void {
-  //   this.getRicetta();
-  // }
-
-  // getRicetta(): void {
-  //   var token = sessionStorage.getItem("token");
-
-  //   if (!token) {
-  //     token = "admin-2";
-  //   }
-
-  //   const headers = new HttpHeaders({
-  //     'Content-Type': 'application/json',
-  //     'token': token
-  //   });
-
-  //   const idRicetta = 2; // Assuming you want to fetch recipe with id 2
-
-  //   this.http.get<Ricetta>("http://localhost:8080/api/recipe/recipeById?id=" + idRicetta, { headers }).subscribe(
-  //     (response: Ricetta) => {
-  //       this.ricetta = response;
-  //     },
-  //     (error) => {
-  //       console.error('Error fetching recipe:', error);
-  //     }
-  //   );
-  // }
 }
