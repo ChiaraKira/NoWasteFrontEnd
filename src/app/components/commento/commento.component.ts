641a990e65a865351134bfbd210d6e67b2e081ca
sapli97 <sarapileio@gmail.com>
2024-04-03 17:20:14 +0200

    migliorie grafiche, inizio logica per commenti, inserito logica lista commenti in dettaglio e rating

--- src/app/components/commento/commento.component.ts
+++ src/app/components/commento/commento.component.ts
@@ -1,4 +1,7 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { Commento } from 'src/app/model/commento';
+import { Ricetta } from 'src/app/model/ricetta';
 
 @Component({
   selector: 'app-commento',
@@ -7,6 +10,36 @@ import { Component } from '@angular/core';
 })
 export class CommentoComponent {
 
+  ris : Commento[] = [];
+  ricetta? : Ricetta[];
+  commento? : Commento[];
+  
+ 
+  constructor(private http: HttpClient) {}
+
+  sendCommentToBackend(): void {
+    const token = sessionStorage.getItem("token");
+    const headers = {'Content-Type' : 'application/json', 'token' : token as string};
+    const body = JSON.stringify;
+
+    this.http.post("http://localhost:8080/api/comment/addComment" + {'headers' : headers},  body).subscribe(risposta => {
+       this.ris  = risposta as Commento[];
+      console.log(this.ris);
+
+     });
+
+
+      // this.http.post('http://localhost:8080/api/comment/allComments', commentData)
+      //     .subscribe(
+      //         (response) => {
+      //             console.log('Commento inviato con successo al backend:', response);
+      //             // Gestisci qui eventuali azioni da eseguire dopo l'invio del commento
+      //         },
+      //         (error) => {
+      //             console.error('Errore durante l\'invio del commento al backend:', error);
+      //         }
+      //     );
+  }
 
 
 }
--- src/app/components/stelle/stelle.component.ts
+++ src/app/components/stelle/stelle.component.ts
@@ -1,46 +1,70 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder } from '@angular/forms';
 import { RatingService } from 'src/app/services/rating.service';
-  
-interface Rating {
-  id: number;
-}
+
+
 
 @Component({
   selector: 'app-stelle',
   templateUrl: './stelle.component.html',
-  styleUrls: ['./stelle.component.css']
-})
-export class StelleComponent implements OnInit {
-  ratings: Rating[] = [
-    { id: 1 },
-    { id: 2 },
-    { id: 3 },
-    { id: 4 },
-    { id: 5 }
-  ];
-  ratingForm!: FormGroup;
-  selectedRating: number | undefined;
-
-  constructor(private formBuilder: FormBuilder, private ratingService: RatingService) { }
-
-  ngOnInit(): void {
-    this.ratingForm = this.formBuilder.group({
-      rating: [null]
-    });
-    this.ratingForm.get('rating')?.valueChanges.subscribe(value => {
-      this.selectedRating = value;
-    });
-  }
-
-  submitRating() {
-    if (this.selectedRating !== undefined) {
-      this.ratingService.submitRating(this.selectedRating).subscribe(() => {
-        // Se necessario, aggiungi qui logica aggiuntiva dopo aver inviato il punteggio al backend
-        console.log('Punteggio inviato con successo al backend');
-      }, error => {
-        console.error('Errore durante l\'invio del punteggio al backend:', error);
-      });
+  styleUrls: ['./stelle.component.css'],
+  template: `
+    <mat-icon *ngFor="let _ of stelleArray()" class="star-icon" [ngClass]="{'checked': isStellaSelezionata(i)}">star</mat-icon>
+  `,
+  styles: [`
+    .star-icon {
+      font-size: 24px;
+      color: #FFD700;
     }
+    .checked {
+      color: #FFD700;
+    }
+  `]
+})
+export class StelleComponent  {
+  // ratings: Rating[] = [
+  //   { id: 1 },
+  //   { id: 2 },
+  //   { id: 3 },
+  //   { id: 4 },
+  //   { id: 5 }
+  // ];
+  // ratingForm!: FormGroup;
+  // selectedRating: number | undefined;
+
+  // constructor(private formBuilder: FormBuilder, private ratingService: RatingService) { }
+
+  // ngOnInit(): void {
+  //   this.ratingForm = this.formBuilder.group({
+  //     rating: [null]
+  //   });
+  //   this.ratingForm.get('rating')?.valueChanges.subscribe(value => {
+  //     this.selectedRating = value;
+  //   });
+  // }
+
+  // submitRating() {
+  //   if (this.selectedRating !== undefined) {
+  //     this.ratingService.submitRating(this.selectedRating).subscribe(() => {
+  //       // Se necessario, aggiungi qui logica aggiuntiva dopo aver inviato il punteggio al backend
+  //       console.log('Punteggio inviato con successo al backend');
+  //     }, error => {
+  //       console.error('Errore durante l\'invio del punteggio al backend:', error);
+  //     });
+  //   }
+  // }
+
+  @Input() punteggio!: number;
+  maxPunteggio: number = 5; // Massimo punteggio, supponendo che il punteggio massimo sia 5
+
+  stelleArray(): number[] {
+    return Array.from({ length: this.maxPunteggio });
+    
   }
+
+  isStellaSelezionata(index: number): boolean {
+    return index < this.punteggio;
+   
+  }
+  
 }
