3a46254db58bae2c24170590666588b3880e5b40
HassanAlSeblani <hassan.alseblani+git@hotmail.com>
2024-04-03 15:53:34 +0200

    bozza form singolo

---
+++ src/app/components/ricetta-ingrediente/ricetta-ingrediente.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ricetta-ingrediente',
+  templateUrl: './ricetta-ingrediente.component.html',
+  styleUrls: ['./ricetta-ingrediente.component.css']
+})
+export class RicettaIngredienteComponent {
+
+}
